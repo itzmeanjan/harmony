1e57a65817322691666ec106707d91a0ceca6e98
Anjan Roy <anjanroy@yandex.com>
2021-02-28 09:32:54 +0530

    set basic ground for running application

---
+++ app/bootup/bootup.go
@@ -0,0 +1,26 @@
+package bootup
+
+import (
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/itzmeanjan/harmony/app/config"
+	"github.com/itzmeanjan/harmony/app/data"
+)
+
+// SetGround - This is to be called when starting application
+// for doing basic ground work(s), so that all required resources
+// are available for further usage during application lifetime
+func SetGround(file string) (*data.Resource, error) {
+
+	if err := config.Read(file); err != nil {
+		return nil, err
+	}
+
+	client, err := ethclient.Dial(config.Get("RPCUrl"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &data.Resource{RPCClient: client}, nil
+
+}
