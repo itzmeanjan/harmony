6e220e29275b4e3a092d8f9e701509f257d3fe4c
Anjan Roy <anjanroy@yandex.com>
2021-02-28 21:04:50 +0530

    using parent context for connecting to RPC node

--- app/bootup/bootup.go
+++ app/bootup/bootup.go
@@ -1,6 +1,8 @@
 package bootup
 
 import (
+	"context"
+
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/itzmeanjan/harmony/app/config"
 	"github.com/itzmeanjan/harmony/app/data"
@@ -9,13 +11,13 @@ import (
 // SetGround - This is to be called when starting application
 // for doing basic ground work(s), so that all required resources
 // are available for further usage during application lifetime
-func SetGround(file string) (*data.Resource, error) {
+func SetGround(ctx context.Context, file string) (*data.Resource, error) {
 
 	if err := config.Read(file); err != nil {
 		return nil, err
 	}
 
-	client, err := ethclient.Dial(config.Get("RPCUrl"))
+	client, err := ethclient.DialContext(ctx, config.Get("RPCUrl"))
 
 	if err != nil {
 		return nil, err
