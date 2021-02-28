4cdcb3c00f445be465702c5c7f2878bfc8f63204
Anjan Roy <anjanroy@yandex.com>
2021-02-28 09:27:48 +0530

    keeping resources in a single struct for ease of passing/ using

---
+++ app/data/resource.go
@@ -0,0 +1,10 @@
+package data
+
+import "github.com/ethereum/go-ethereum/ethclient"
+
+// Resource - Shared resources among multiple go routines
+//
+// Needs to be released carefully when shutting down
+type Resource struct {
+	RPCClient *ethclient.Client
+}
