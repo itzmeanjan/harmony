b840d651f5fbeb1bf76413a15cba0077db375206
Anjan Roy <anjanroy@yandex.com>
2021-02-28 09:29:32 +0530

    easy to use function for releasing shared resources when shutting down

--- app/data/resource.go
+++ app/data/resource.go
@@ -8,3 +8,11 @@ import "github.com/ethereum/go-ethereum/ethclient"
 type Resource struct {
 	RPCClient *ethclient.Client
 }
+
+// Release - To be called when application will receive shut down request
+// from system, to gracefully deallocate all resources
+func (r *Resource) Release() {
+
+	r.RPCClient.Close()
+
+}
