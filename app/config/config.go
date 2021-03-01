27b0046acdec4c96c192e5317a3de685e19c7637
Anjan Roy <anjanroy@yandex.com>
2021-03-01 08:56:15 +0530

    new function for reading mempool polling period from `.env` file

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"log"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
 
 // Read - Reading .env file content, during application start up
 func Read(file string) error {
@@ -12,4 +17,24 @@ func Read(file string) error {
 // Get - Get config value by key
 func Get(key string) string {
 	return viper.GetString(key)
+}
+
+// GetMemPoolPollingPeriod - Read mempool polling period & attempt to
+// parse it to string, where it's expected that this period will be
+// provided in form of time duration with millisecond level precision
+//
+// Example: If you want to poll mempool content every 2 seconds, you must be
+// writing 2000 in `.env` file
+func GetMemPoolPollingPeriod() uint64 {
+
+	period := Get("MemPoolPollingPeriod")
+
+	_period, err := strconv.ParseUint(period, 10, 64)
+	if err != nil {
+		log.Printf("[❗️] Failed to parse mempool polling period : %s\n", err.Error())
+		return 0
+	}
+
+	return _period
+
 }
