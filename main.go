e48e3389cc4ef24287df9968be1ef48cf1375c94
Anjan Roy <anjanroy@yandex.com>
2021-03-01 09:08:52 +0530

    spawning go routine for polling current mempool content

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/itzmeanjan/harmony/app/bootup"
+	"github.com/itzmeanjan/harmony/app/txpool"
 )
 
 func main() {
@@ -39,6 +40,7 @@ func main() {
 	// Attempt to catch interrupt event(s)
 	// so that graceful shutdown can be performed
 	interruptChan := make(chan os.Signal, 1)
+	comm := make(chan struct{}, 1)
 	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
 
 	go func() {
@@ -55,21 +57,44 @@ func main() {
 
 		}()
 
-		// Attempting to catch OS interrupt
-		// sent to `harmony`
-		<-interruptChan
+		for {
 
-		// When interrupt is received, attempting to
-		// let all other go routines know, master go routine
-		// wants all to shut down, they must do a graceful shut down
-		// of what they're doing now
-		cancel()
+			select {
 
-		// Giving workers 3 seconds, before forcing shutdown
-		//
-		// This is simply a blocking call i.e. blocks for 3 seconds
-		<-time.After(time.Second * time.Duration(3))
+			case <-interruptChan:
+
+				// When interrupt is received, attempting to
+				// let all other go routines know, master go routine
+				// wants all to shut down, they must do a graceful shut down
+				// of what they're doing now
+				cancel()
+
+				// Giving workers 3 seconds, before forcing shutdown
+				//
+				// This is simply a blocking call i.e. blocks for 3 seconds
+				<-time.After(time.Second * time.Duration(3))
+
+			case <-comm:
+				// Supervisor go routine got to learn
+				// that go routine it spawned some time ago
+				// for polling ethereum mempool content periodically
+				// has died
+				//
+				// It's supposed to spawn new go routine for handling that op
+				//
+				// @note To be implemented
+
+			}
+
+		}
 
 	}()
+
+	go txpool.PollTxPoolContent(ctx, resources, comm)
+
+	// This is just a fancy of blocking execution, so that
+	// main go routine doesn't die & program keeps running
+	stop := make(chan struct{})
+	<-stop
 
 }
