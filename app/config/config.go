6a78eae86c5188d079421450fa438dca17e3ce4a
Anjan Roy <anjanroy@yandex.com>
2021-03-01 08:59:39 +0530

    using default 1000ms period for polling, if nothing is provided

--- app/config/config.go
+++ app/config/config.go
@@ -25,14 +25,18 @@ func Get(key string) string {
 //
 // Example: If you want to poll mempool content every 2 seconds, you must be
 // writing 2000 in `.env` file
+//
+// If you don't provide any value for this expected field, by default it'll
+// start using 1000ms i.e. after completion of this iteration, it'll sleep for
+// 1000ms & again get to work
 func GetMemPoolPollingPeriod() uint64 {
 
 	period := Get("MemPoolPollingPeriod")
 
 	_period, err := strconv.ParseUint(period, 10, 64)
 	if err != nil {
-		log.Printf("[❗️] Failed to parse mempool polling period : %s\n", err.Error())
-		return 0
+		log.Printf("[❗️] Failed to parse mempool polling period : `%s`, using 1000 ms\n", err.Error())
+		return 1000
 	}
 
 	return _period
