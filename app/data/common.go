8d769221ccd6b08469e8dcc6ab4242b984291c98
Anjan Roy <anjanroy@yandex.com>
2021-03-04 10:49:20 +0530

    check presence of txhash in pending/ queued pool

---
+++ app/data/common.go
@@ -0,0 +1,32 @@
+package data
+
+import "github.com/ethereum/go-ethereum/common"
+
+// IsPresentInCurrentPool - Given tx hash, which was previously present in pending/ queued pool
+// attempts to check whether it's present in current txpool content or not
+//
+// @note `txs` is current pending/ queued pool state received over
+// RPC interface
+func IsPresentInCurrentPool(txs map[string]map[string]*MemPoolTx, txHash common.Hash) bool {
+
+	var present bool
+
+	{
+	OUTER:
+		for _, vOuter := range txs {
+			for _, vInner := range vOuter {
+
+				if vInner.Hash == txHash {
+
+					present = true
+					break OUTER
+
+				}
+
+			}
+		}
+	}
+
+	return present
+
+}
