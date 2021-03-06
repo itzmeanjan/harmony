7a41ad9ce1a83e040f33053ff523f6d5e9e8161b
Anjan Roy <anjanroy@yandex.com>
2021-03-06 08:04:51 +0530

    http response

---
+++ app/data/response.go
@@ -0,0 +1,8 @@
+package data
+
+// Stat - Response to client queries for current mempool state
+// to be sent in this form
+type Stat struct {
+	PendingPoolSize uint64 `json:"pendingPoolSize"`
+	QueuedPoolSize  uint64 `json:"queuedPoolSize"`
+}
