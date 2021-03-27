67e1fcd99d107e37e6e9f646a8c505c513e1081e
Anjan Roy <anjanroy@yandex.com>
2021-03-27 09:23:54 +0530

    slight change in peer reconnection attempt info holder struct

--- app/networking/reconnect.go
+++ app/networking/reconnect.go
@@ -4,19 +4,31 @@ import (
 	"context"
 	"time"
 
+	"github.com/itzmeanjan/harmony/app/config"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
+
+// AttemptDetails - Keeps track of how many times we attempted to
+// connected to certain peer
+//
+// These are to be refreshed after each failed attempt
+type AttemptDetails struct {
+	StartedAt       time.Time
+	LastAttemptedAt time.Time
+	Times           uint64
+	Delay           time.Duration
+}
 
 // ReconnectionManager - When stream to peer gets reset, it can be attempted
 // to be reestablished, by sending request to this worker
 //
 // All peer related information required for doing so, will be kept in this struct
 type ReconnectionManager struct {
-	Host        host.Host
-	Peers       map[peer.ID]time.Time
-	WaitingTime time.Duration
-	NewPeerChan chan peer.ID
+	Host            host.Host
+	Peers           map[peer.ID]*AttemptDetails
+	MaxAttemptCount uint64
+	NewPeerChan     chan peer.ID
 }
 
 // Start - ...
@@ -36,10 +48,13 @@ func (r *ReconnectionManager) Start(ctx context.Context) {
 
 }
 
-// NewReconnectionManager - ...
-func NewReconnectionManager() *ReconnectionManager {
+// NewReconnectionManager - Creates a new instance of peer reconnection
+// attempt manager
+func NewReconnectionManager(_host host.Host) *ReconnectionManager {
 	return &ReconnectionManager{
-		Peers:       make(map[peer.ID]time.Time),
-		WaitingTime: time.Duration(1) * time.Minute,
+		Host:            _host,
+		Peers:           make(map[peer.ID]*AttemptDetails),
+		MaxAttemptCount: config.GetMaxReconnectionAttemptCount(),
+		NewPeerChan:     make(chan peer.ID, 10),
 	}
 }
