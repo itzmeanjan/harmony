ef2db0b4cd0797af053773362583be53475fb22c
Anjan Roy <anjanroy@yandex.com>
2021-03-27 10:14:27 +0530

    every 1000ms drop peer ids which are not required to kept any more

--- app/networking/reconnect.go
+++ app/networking/reconnect.go
@@ -26,6 +26,13 @@ func (a *AttemptDetails) EverAttempted() bool {
 	return a.Times > 0
 }
 
+// ShouldWeDrop - If all allowed attempts exhausted, we should
+// not reattempt, we hope that peer will find us/ we'll via
+// rendezvous
+func (a *AttemptDetails) ShouldWeDrop(maxAttempt uint64) bool {
+	return a.Times > maxAttempt
+}
+
 // ShallWeAttempt - If never attempted, surely we can attempt
 //
 // If we've exhausted all attempts, we'll not reattempt, they're to
@@ -83,6 +90,26 @@ func (r *ReconnectionManager) Start(ctx context.Context) {
 			}
 
 		case <-time.After(time.Duration(100) * time.Millisecond):
+
+		case <-time.After(time.Duration(1000) * time.Millisecond):
+			// Every 1 seconds, we'll attempt to check if any peer
+			// we've enlisted, which are not required to be retried
+			// anymore in future, so we can drop those
+
+			eligibles := make([]peer.ID, 0, len(r.Peers))
+
+			for k, v := range r.Peers {
+
+				if v.ShouldWeDrop(r.MaxAttemptCount) {
+					eligibles = append(eligibles, k)
+				}
+
+			}
+
+			// If found any to be eligible, attempt to drop those
+			for _, v := range eligibles {
+				delete(r.Peers, v)
+			}
 
 		}
 
