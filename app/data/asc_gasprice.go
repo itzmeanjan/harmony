60be0cd39fa9e6926e78dcaf1fd106bdc64b5968
Anjan Roy <anjanroy@yandex.com>
2021-03-30 13:33:14 +0530

    txs sorted in ascending order as per gas price paid

---
+++ app/data/asc_gasprice.go
@@ -0,0 +1,64 @@
+package data
+
+// MemPoolTxsAsc - List of mempool tx(s)
+//
+// @note This structure to be used for sorting tx(s)
+// in ascending way, using gas price they're paying
+type MemPoolTxsAsc []*MemPoolTx
+
+// findInsertionPoint - Find index at which newly arrived tx should be entered to
+// keep this slice sorted, where it's sorted descendically as per gas price paid
+func (m MemPoolTxsAsc) findInsertionPoint(low int, high int, tx *MemPoolTx) int {
+
+	if low > high {
+		return 0
+	}
+
+	if low == high {
+
+		if BigHexToBigDecimal(m[low].GasPrice).Cmp(BigHexToBigDecimal(tx.GasPrice)) > 0 {
+			return low
+		}
+
+		return low + 1
+
+	}
+
+	mid := (low + high) / 2
+	if BigHexToBigDecimal(m[mid].GasPrice).Cmp(BigHexToBigDecimal(tx.GasPrice)) > 0 {
+
+		return m.findInsertionPoint(low, mid, tx)
+
+	}
+
+	return m.findInsertionPoint(mid+1, high, tx)
+
+}
+
+// findTx - Find index of tx, which is already present in this slice, where
+// txs are sorted descendically as per gas price paid
+func (m MemPoolTxsAsc) findTx(low int, high int, tx *MemPoolTx) int {
+
+	if low > high {
+		return -1
+	}
+
+	if low == high {
+
+		idx := findTxFromSlice(m[low:], tx)
+		if idx == -1 {
+			return -1
+		}
+
+		return low + idx
+
+	}
+
+	mid := (low + high) / 2
+	if BigHexToBigDecimal(m[mid].GasPrice).Cmp(BigHexToBigDecimal(tx.GasPrice)) >= 0 {
+		return m.findTx(low, mid, tx)
+	}
+
+	return m.findTx(mid+1, high, tx)
+
+}
