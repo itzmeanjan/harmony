12e0fb6970fff3ce92bd089ce2f4fb45b895aee0
Anjan Roy <anjanroy@yandex.com>
2021-04-18 09:42:13 +0530

    tracker for immediately not found txs during pending pool pruning

---
+++ app/data/not_found_txs.go
@@ -0,0 +1,61 @@
+package data
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/itzmeanjan/harmony/app/listen"
+)
+
+// AlreadyMinedTx - Pending pool pruner to be informed about some tx
+// it considers as pending, is actually mined, some time, which pool missed
+// because tx was added into pool, after block was mined, due to some
+// unexpected network propagation delay
+type AlreadyMinedTx struct {
+	Hash  common.Hash
+	Nonce uint64
+}
+
+// TrackNotFoundTxs - Those tx(s) which are found to be mined before actual pending tx is added
+// into mempool, are kept track of here & pending pool pruner is informed about it, when it's found
+// to be joining mempool, in some time future.
+func TrackNotFoundTxs(ctx context.Context, inPendingPoolChan <-chan *MemPoolTx, notFoundTxsChan <-chan listen.CaughtTxs, alreadyMinedTxChan chan<- AlreadyMinedTx) {
+
+	// Yes, for faster lookup, it's kept this
+	keeper := make(map[common.Hash]*listen.CaughtTx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case tx := <-inPendingPoolChan:
+			// Just learnt about new tx which is considered to be pending
+			// & added into pool, but this tx is mined in some previous block
+			// which we've kept track of here in `keeper` structure
+			//
+			// We're letting pending pool pruner know, it must act in declaring this tx
+			// to be mined
+
+			if kept, ok := keeper[tx.Hash]; ok {
+				alreadyMinedTxChan <- AlreadyMinedTx{Hash: kept.Hash, Nonce: kept.Nonce}
+				delete(keeper, tx.Hash)
+			}
+
+		case txs := <-notFoundTxsChan:
+			// New block was mined with some txs
+			// which were not found in pending pool then,
+			// which are being kept track of here
+			//
+			// To be used for letting pending pool know
+			// some newly added pending pool tx is actually mined
+			// in some later date
+
+			for i := 0; i < len(txs); i++ {
+				keeper[txs[i].Hash] = txs[i]
+			}
+
+		}
+	}
+
+}
